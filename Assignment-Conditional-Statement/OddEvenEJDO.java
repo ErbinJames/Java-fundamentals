057437aa64ab1981e65fc2b7a2c77fe63fb8356d
ErbinJames <eorodio04922@student.dmmmsu.edu.ph>
2025-03-19 01:03:40 +0800

    creating Oddeven task

---
+++ Assignment-Conditional-Statement/OddEvenEJDO.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class OddEvenEJDO {
+    public static void main(String[] args){
+        //create a scanner object 
+        Scanner scanner = new Scanner(System.in);
+
+        //declaring variable to store user input
+        int number;
+
+        // prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        number = scanner.nextInt();
+
+        //check if the number is even or odd
+        if (number %2 == 0) {
+            System.out.println(number + " is an even number."); //even
+        } else {
+            System.out.println(number + " is an odd number.");//odd
+        }
+
+        scanner.close(); //close the scanner to erase or free resources
+    }
+}
