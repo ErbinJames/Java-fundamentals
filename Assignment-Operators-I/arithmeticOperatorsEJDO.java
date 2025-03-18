9ba38487a093dc4017da7b8cbd05e8a35f3ccd47
ErbinJames <eorodio04922@student.dmmmsu.edu.ph>
2025-03-18 11:53:12 +0800

    pushing first activity

---
+++ Assignment-Operators-I/arithmeticOperatorsEJDO.java
@@ -0,0 +1,11 @@
+//Task 1: Arithmetic Operations
+public class arithmeticOperatorsEJDO {
+    public static void main (String[] args) {
+        float a = 20.55f, b = 10.15f;
+
+        System.out.printf("Addition: %.2f\n", (a + b));
+        System.out.printf("Subtraction: %.2f\n", (a - b));
+        System.out.printf("Multiplication: %.2f\n", (a * b));
+        System.out.printf("Division: %.2f\n", (a / b));
+    }
+}
